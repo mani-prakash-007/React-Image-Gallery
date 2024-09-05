4132ec37ce2087397b2431c042e6058cbcb2726b
mani-prakash-007 <manip8072@gmail.com>
2024-09-05 18:02:11 +0530

    Added Fetch functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="my-20">
         <h1 className="font-bold text-4xl text-center">Image Gallery</h1>
-        <div className="flex justify-center border my-10 mx-40">
+        <div className="flex justify-center my-10 mx-auto">
           <ImageGallery />
         </div>
       </div>
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,24 +1,100 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+
+//Keys
+const API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
 
 export const ImageGallery = () => {
+  const [image, setImage] = useState();
+  const [totalPages, setTotalPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+  const [currentInputValue, setCurrentInputValue] = useState();
+  const inputRef = useRef();
+
+  //Function for fetching Images
+
+  const handleClick = async (pageNumber) => {
+    const inputData = inputRef.current.value;
+    const url = `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${inputData}&client_id=${API_KEY}`;
+    const response = await axios.get(url);
+    setImage(response.data);
+    setTotalPages(response.data.total_pages);
+    setCurrentInputValue(inputData);
+  };
+
+  //Function for Show more
+  const handleShowMore = async (page) => {
+    const url = `https://api.unsplash.com/search/photos?page=${
+      page + 1
+    }&query=${currentInputValue}&client_id=${API_KEY}`;
+    const response = await axios.get(url);
+    const newData = response.data.results;
+    setImage((previousData) => ({
+      ...previousData,
+      results: [...previousData.results, ...newData],
+    }));
+    setPageNumber(pageNumber + 1);
+  };
   return (
     <>
-      <div className="flex flex-col justify-center border border-black p-5 w-full">
-        <div className="flex justify-center border border-black p-5 my-5">
+      <div className="flex flex-col justify-center w-full">
+        <div className="flex justify-center p-5">
           <input
+            ref={inputRef}
             type="text"
-            className="border border-black h-10 mx-5 w-80 rounded-xl"
+            className="border border-black h-10 px-5 mx-5 w-80 rounded-xl focus:outline-none "
+            placeholder="Search for Images"
           />
-          <button className="border border-black mx-5 px-5 rounded-xl active:scale-90">
+          <button
+            onClick={() => handleClick(pageNumber)}
+            className="border border-black mx-5 px-5 rounded-xl active:scale-90"
+          >
             Search
           </button>
         </div>
-        <div className=" flex justify-evenly flex-wrap border border-black p-5 my-5">
-          <div className="border border-black w-80 h-96 m-5 rounded-xl "></div>
-          <div className="border border-black w-80 h-96 m-5 rounded-xl "></div>
-          <div className="border border-black w-80 h-96 m-5 rounded-xl "></div>
-          <div className="border border-black w-80 h-96 m-5 rounded-xl "></div>
+        <div className=" flex justify-evenly flex-wrap p-5 ">
+          {!image ? (
+            <h1 className="text-center font-medium text-2xl">
+              Search for Images...😊
+            </h1>
+          ) : image.total_pages === 0 ? (
+            <h1 className="text-center font-medium text-2xl">
+              No Images found...😔
+            </h1>
+          ) : (
+            image.results &&
+            image.results.map((data, index) => {
+              return (
+                <div
+                  key={index}
+                  className="border border-black w-96 min-h-96 m-5 rounded-xl shadow-[1px_4px_25px_10px_#a0aec0]"
+                >
+                  <a href={data.links.html} target="_blank">
+                    <img
+                      src={data.urls.small}
+                      alt="avengers"
+                      className=" rounded-t-lg h-56 w-full hover:opacity-80"
+                    />
+                  </a>
+                  <p className="px-5 py-5 font-thin text-2xl">
+                    {data.alt_description}
+                  </p>
+                </div>
+              );
+            })
+          )}
         </div>
+        {image &&
+          (image.total_pages != 0 || image.total_pages < totalPages) && (
+            <div className="flex justify-center my-10">
+              <button
+                onClick={() => handleShowMore(pageNumber)}
+                className="border px-5 py-2 w-40 rounded-2xl font-bold text-xl text-sky-500 active:scale-95 hover:text-white hover:bg-sky-500"
+              >
+                Show More
+              </button>
+            </div>
+          )}
       </div>
     </>
   );
